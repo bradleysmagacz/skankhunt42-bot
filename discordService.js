99da5824597827c5f931a8b45257a09ec9df24cc
Bradley Smagacz <bradley.smagacz@bsmagacz.local>
2018-01-19 09:59:31 -0500

    changed to object oriented approach

---
+++ discordService.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const auth = require('./auth.json');
+
+class DiscordService {
+  constructor() {
+    this.discordApiBase = 'https://discordapp.com/api';
+    this.authHeader = {
+      Authorization: 'Bot ' + auth.token
+    };
+  }
+
+  get(url) {
+    return axios({
+      method: 'GET',
+      url: `${this.discordApiBase}/${url}`,
+      headers: this.authHeader
+    }).then(res => res.data);
+  }
+}
+
+module.exports = new DiscordService();
